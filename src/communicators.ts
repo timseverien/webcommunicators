ac174d80e9c7022cee400e873d0aa71c64471e02
Tim Severien <timseverien@gmail.com>
2023-02-12 07:54:43 +0100

    Add Hidde de Vries

--- src/communicators.ts
+++ src/communicators.ts
@@ -2,6 +2,26 @@ import type { Communicator } from './communicator.js';
 
 export const COMMUNICATORS: Communicator[] = [
 	{
+		name: 'Hidde de Vries',
+		languages: ['en'],
+		channels: [
+			{
+				type: 'BLOG',
+				url: 'https://hidde.blog/blog/',
+				feedUrl: 'https://hidde.blog/feed',
+			},
+			{
+				type: 'MASTODON',
+				serverDomain: 'front-end.social',
+				username: 'hdv',
+			},
+			{
+				type: 'TWITTER',
+				username: 'hdv',
+			},
+		],
+	},
+	{
 		name: 'Tim Severien',
 		languages: ['en'],
 		channels: [
